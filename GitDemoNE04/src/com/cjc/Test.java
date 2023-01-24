1a83de89cd490896535035c508455c188fae062f
PawarKanchan1 <123442477+PawarKanchan1@users.noreply.github.com>
2023-01-24 15:10:01 +0530

    Update Test.java
    AC

--- GitDemoNE04/src/com/cjc/Test.java
+++ GitDemoNE04/src/com/cjc/Test.java
@@ -6,6 +6,9 @@ public class Test
 	{
 		System.out.println("Hello");
 		System.out.println("Hello Git");
+		System.setProperty("webdriver.chrome.driver","C:\Users\user\Desktop\seleniumjar\109\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/register");
 	}
 
 }
